bb622ff28a8797c324d4672346f4bb19fc003d35
Lars Thielemann <LT>
2025-08-31 15:33:53 +0200

    app commit -- env vars

--- config.py
+++ config.py
@@ -5,9 +5,8 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secretkey'
-    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') #or \
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://DBWE_User:JBa673%4000aj@localhost/DBWE_app' #or \
-                                                             #'sqlite:///' + os.path.join(basedir, 'app.db')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
+                                                            
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
