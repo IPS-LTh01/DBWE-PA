19fefc418142cb3554b0bec49e3c0f4a9093367b
Lars Thielemann <LT>
2025-08-31 15:51:06 +0200

    app commit -- env flaskenv

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 # app/config.py
 
 import os
-
 basedir = os.path.abspath(os.path.dirname(__file__))
+
+from dotenv import load_dotenv
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
